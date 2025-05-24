d31d32f40b7ef5c15eeab8534df8d4756cc79d41
ashishgithuba <aks0601004@gmail.com>
2025-05-24 22:45:26 +0530

    Hoisting

--- InterviewcodeingQuestions/01.js
+++ InterviewcodeingQuestions/01.js
@@ -1,4 +1,8 @@
-console.log("My age is ", age)
-var age=20;
-console.log("My age is ",age);
+// console.log("My age is ", age)
+// var age=20;
+// console.log("My age is ",age);
 //This behavior is due to JavaScript's hoisting mechanism.
+console.log("My age is ", age); // ReferenceError
+let age = 20;
+console.log("My age is ", age);
+//ReferenceError: Cannot access 'age' before initialization
